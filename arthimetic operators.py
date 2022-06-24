803f91372013f269379b8c2ee94baeb619c6f4be
Alishahwaiz <ali.shaviaz525@gmail.com>
2022-06-23 18:25:14 -0700

    arithemetic operators with if statement
    created locally

---
+++ arthimetic operators.py
@@ -0,0 +1,9 @@
+
+price= int(input("enter price "))
+print(type(price))
+bad_history = True
+if bad_history:
+    print("you have bad history, you have only 10 poerncent discount")
+    discount= price*10/100
+    price = price-discount
+    print("your price will be",price)
